1dfebb5892ed7eaec2e3af38a25f11918bf1e94b
catiqueue <catiqueue@gmail.com>
2025-08-16 12:05:05 +0300

    Ported Paint.XOR to Paint.NET 5

--- PaintDotXor/src/LolK.PaintDotNet.Plugins.PaintDotXor.cs
+++ PaintDotXor/src/LolK.PaintDotNet.Plugins.PaintDotXor.cs
@@ -1,144 +1,193 @@
 ﻿using System;
 using System.Drawing;
-
-using System.Reflection;
-
+using System.Globalization;
 using PaintDotNet;
 using PaintDotNet.Effects;
+using PaintDotNet.Imaging;
 using PaintDotNet.IndirectUI;
 using PaintDotNet.PropertySystem;
+using IVec2 = PaintDotNet.Rendering.Vector2Int32;
 
-[assembly: AssemblyTitle("Paint.XOR")]
-[assembly: AssemblyDescription("XOR pattern effect for Paint.NET")]
-[assembly: AssemblyCompany("catiqueue")]
-[assembly: AssemblyProduct("Paint.XOR")]
-[assembly: AssemblyCopyright("Copyright © catiqueue 2021")]
-[assembly: AssemblyCulture("en-US")]
-[assembly: System.Runtime.InteropServices.ComVisible(false)]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: System.Runtime.Versioning.TargetPlatform("Windows7.0")]
-[assembly: System.Runtime.Versioning.SupportedOSPlatform("Windows7.0")]
+namespace LolK.PaintDotNet.Plugins;
 
-namespace LolK.PaintDotNet.Plugins {
-  public class PluginSupportInfo : IPluginSupportInfo {
-    private readonly Assembly assembly = typeof(PluginSupportInfo).Assembly;
-    public string Author => assembly.GetCustomAttribute<AssemblyCopyrightAttribute>().Copyright;
-    public string Copyright => assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
-    public string DisplayName => assembly.GetCustomAttribute<AssemblyProductAttribute>().Product;
-    public Version Version => assembly.GetName().Version;
-    public Uri WebsiteUri => new Uri("https://github.com/catiqueue");
-    // YEP. I'm an asshole. I've hardcoded a picture.
-    public Image Icon => Image.FromStream(new System.IO.MemoryStream(Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsEAAA7BAbiRa+0AAABVSURBVDhPfYpBDsAwCMP4/6fZJtSGhs4+RAQnXjJzZ0Efcgv/9N53hVvvJ2Snmyl4Vyl65/V3e+e1X7e1W++87vytewreVQpy03o/udu5pg+5hT4RD2PJHvDN2VqUAAAAAElFTkSuQmCC"), 0, 192), true);
-    public string SubMenu => SubmenuNames.Render;
-    public System.Globalization.CultureInfo Culture => assembly.GetName().CultureInfo;
+internal static class Constants {
+  public const string EffectName = "Paint.XOR";
+  public const string Description = "XOR pattern effect for Paint.NET";
+  public const string Version = "1.4.0";
+  public const string Author = "catiqueue";
+  public const string Culture = "en-US";
+  public const string Website = "https://github.com/catiqueue";
+  public const string Copyright = $"Copyright © {Author} 2025";
+  public const string Base64Image = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsEAAA7BAbiRa+0AAABVSURBVDhPfYpBDsAwCMP4/6fZJtSGhs4+RAQnXjJzZ0Efcgv/9N53hVvvJ2Snmyl4Vyl65/V3e+e1X7e1W++87vytewreVQpy03o/udu5pg+5hT4RD2PJHvDN2VqUAAAAAElFTkSuQmCC";
+  public const int Base64ImageStringLength = 192;
+}
+
+internal delegate bool Filter(int magic);
+internal delegate ManagedColor Painter(int magic);
+internal delegate int Operation(IVec2 pos);
+internal enum OperationChoice { XOR, AND, OR }
+internal enum FilterChoice { IsPrime, IsDivisible }
+
+internal static class Create {
+  public static class Filter {
+    public static Plugins.Filter IsPrime => MathHelpers.IsPrime;
+    public static Plugins.Filter DivisibleBy(int divisor) => magic => MathHelpers.IsDivisible(magic, divisor);
+  }
+  public static class Painter {
+    public static Plugins.Painter SineHsvPainter => magic => ManagedColor.Create((SrgbColorA) new ColorHsv96Float(
+      hue: MathF.Sin(magic) * 180f + 180f,
+      saturation: 100f,
+      value: MathF.Cos(magic) * 50f + 50f).ToRgb());
+    public static Plugins.Painter ConstantColorPainter(ManagedColor color) => _ => color;
+  }
+  public static class Operation {
+    public static Plugins.Operation XOR => pos => pos.X ^ pos.Y;
+    public static Plugins.Operation AND => pos => pos.X & pos.Y;
+    public static Plugins.Operation OR => pos => pos.X | pos.Y;
+    public static Plugins.Operation FromChoice(OperationChoice choice) => choice switch {
+      OperationChoice.XOR => XOR,
+      OperationChoice.AND => AND,
+      OperationChoice.OR => OR,
+      _ => throw new ArgumentOutOfRangeException(nameof(choice), "Unknown operation choice")
+    };
+  }
+}
+
+internal readonly record struct CameraParameters(IVec2 Offset, int Zoom) {
+  public IVec2 ApplyTo(IVec2 pos) => pos / Zoom + Offset;
+}
+internal sealed class PaintDotXorSettings {
+  public Operation Operation { get; set; } = Create.Operation.XOR;
+  public Filter Filter { get; set; } = Create.Filter.IsPrime;
+  public Painter Painter { get; set; } = Create.Painter.SineHsvPainter;
+  public CameraParameters CameraParameters { get; set; } = new(new IVec2(0, 0), 1);
+}
+
+[PluginSupportInfo(typeof(PluginSupportInfo))]
+public class PaintDotXor() : PropertyBasedBitmapEffect(Info.DisplayName, Info.Icon, PluginSupportInfo.SubMenu, BitmapEffectOptionsFactory.Create() with { IsConfigurable = true }) {
+  private static readonly PluginSupportInfo Info = new();
+  private PaintDotXorSettings Settings { get; } = new();
+
+  private enum PropertyNames { OffsetX, OffsetY, Zoom, FilterMode, Divisor, Operation, UseHSV, Color }
+
+  protected override void OnSetToken(PropertyBasedEffectConfigToken? newToken) {
+    if(newToken == null) return;
+    
+    Settings.CameraParameters = new CameraParameters(
+      Offset: new IVec2(
+        newToken.GetProperty<Int32Property>(PropertyNames.OffsetX)!.Value, 
+        newToken.GetProperty<Int32Property>(PropertyNames.OffsetY)!.Value),
+      Zoom: newToken.GetProperty<Int32Property>(PropertyNames.Zoom)!.Value
+    );
+    
+    Settings.Operation = Create.Operation.FromChoice((OperationChoice)newToken.GetProperty<StaticListChoiceProperty>(PropertyNames.Operation)!.Value);
+    
+    Settings.Filter = (FilterChoice) newToken.GetProperty<StaticListChoiceProperty>(PropertyNames.FilterMode)!.Value switch {
+      FilterChoice.IsDivisible => Create.Filter.DivisibleBy(newToken.GetProperty<Int32Property>(PropertyNames.Divisor)!.Value),
+      FilterChoice.IsPrime => Create.Filter.IsPrime,
+      _ => throw new ArgumentOutOfRangeException(nameof(newToken), "Unknown filter choice!")
+    };
+
+    Settings.Painter = newToken.GetProperty<BooleanProperty>(PropertyNames.UseHSV)!.Value
+      ? Create.Painter.SineHsvPainter
+      : Create.Painter.ConstantColorPainter(newToken.GetProperty<ManagedColorProperty>(PropertyNames.Color)!.Value);
+    
+    base.OnSetToken(newToken);
   }
 
-  [PluginSupportInfo(typeof(PluginSupportInfo))]
-  public class PaintDotXor : PropertyBasedEffect {
-    private static PluginSupportInfo Info = new PluginSupportInfo();
-    private int OffsetX = 0, OffsetY = 0;
-    private int Zoom = 1;
-    private byte CheckMode = 0, BitMode = 0;
-    private int Divisor = 1;
-    private bool UseHSV = true;
-    private ColorBgra Color = ColorBgra.Black;
-    
-    public PaintDotXor() : base(Info.DisplayName, Info.Icon, Info.SubMenu, new EffectOptions() { Flags = EffectFlags.Configurable }) { }
+  protected override void OnRender(IBitmapEffectOutput output) {
+    // should I dispose this? I think it's managed by Paint.NET
+    /* using */ var source = Environment.GetSourceBitmapBgra32();
+    using var sourceLock = source.Lock(source.Bounds());
+    var sourceRegion = sourceLock.AsRegionPtr();
+    using var outputLock = output.LockBgra32();
+    var outputRegion = outputLock.AsRegionPtr();
+    var pos = new IVec2();
 
-    private enum PropertyNames { OffsetX, OffsetY, Zoom, CheckMode, Divisor, BitMode, UseHSV, Color }
-    private enum CheckModeOptions { IsPrime, IsDivisible }
-    private enum BitModeOptions { XOR, AND, OR } 
-
-    protected override PropertyCollection OnCreatePropertyCollection() {
-      Property[] Properties = new Property[] {
-        new Int32Property(PropertyNames.OffsetX, 0, 0, ushort.MaxValue),
-        new Int32Property(PropertyNames.OffsetY, 0, 0, ushort.MaxValue),
-        new Int32Property(PropertyNames.Zoom, 1, 1, 128), // NEW
-        StaticListChoiceProperty.CreateForEnum<CheckModeOptions>(PropertyNames.CheckMode, 0, false),
-        new Int32Property(PropertyNames.Divisor, 1, 1, 512),
-        StaticListChoiceProperty.CreateForEnum<BitModeOptions>(PropertyNames.BitMode, 0, false),
-        new BooleanProperty(PropertyNames.UseHSV, true),
-        new Int32Property(PropertyNames.Color, unchecked((int)EnvironmentParameters.PrimaryColor.Bgra), int.MinValue, int.MaxValue)
-      };
-
-      PropertyCollectionRule[] Rules = new PropertyCollectionRule[] {
-        new ReadOnlyBoundToValueRule<object,StaticListChoiceProperty>(PropertyNames.Divisor, PropertyNames.CheckMode, CheckModeOptions.IsPrime, false),
-        new ReadOnlyBoundToBooleanRule(PropertyNames.Color, PropertyNames.UseHSV, false)
-      };
-
-      return new PropertyCollection(Properties, Rules);
-    }
-
-    protected override ControlInfo OnCreateConfigUI(PropertyCollection Properties) {
-      ControlInfo ConfigUI = CreateDefaultConfigUI(Properties);
-
-      ConfigUI.SetPropertyControlValue(PropertyNames.OffsetX, ControlInfoPropertyNames.DisplayName, "X offset");
-      ConfigUI.SetPropertyControlValue(PropertyNames.OffsetY, ControlInfoPropertyNames.DisplayName, "Y offset");
-
-      ConfigUI.SetPropertyControlValue(PropertyNames.Zoom, ControlInfoPropertyNames.DisplayName, "Zoom");
-
-      ConfigUI.SetPropertyControlValue(PropertyNames.CheckMode, ControlInfoPropertyNames.DisplayName, "Check mode");
-      ConfigUI.FindControlForPropertyName(PropertyNames.CheckMode).SetValueDisplayName(CheckModeOptions.IsPrime, "Is prime?");
-      ConfigUI.FindControlForPropertyName(PropertyNames.CheckMode).SetValueDisplayName(CheckModeOptions.IsDivisible, "Is divisible?");
-
-      ConfigUI.SetPropertyControlValue(PropertyNames.Divisor, ControlInfoPropertyNames.DisplayName, "Divider");
-
-      ConfigUI.SetPropertyControlValue(PropertyNames.BitMode, ControlInfoPropertyNames.DisplayName, "Bit mode");
-      ConfigUI.FindControlForPropertyName(PropertyNames.BitMode).SetValueDisplayName(BitModeOptions.XOR, "XOR");
-      ConfigUI.FindControlForPropertyName(PropertyNames.BitMode).SetValueDisplayName(BitModeOptions.AND, "AND");
-      ConfigUI.FindControlForPropertyName(PropertyNames.BitMode).SetValueDisplayName(BitModeOptions.OR, "OR");
-
-      ConfigUI.SetPropertyControlValue(PropertyNames.UseHSV, ControlInfoPropertyNames.DisplayName, "");
-      ConfigUI.SetPropertyControlValue(PropertyNames.UseHSV, ControlInfoPropertyNames.Description, "Use HSV conversion");
-
-      ConfigUI.SetPropertyControlValue(PropertyNames.Color, ControlInfoPropertyNames.DisplayName, "Pattern color");
-      ConfigUI.SetPropertyControlType(PropertyNames.Color, PropertyControlType.ColorWheel);
-      ConfigUI.SetPropertyControlValue(PropertyNames.Color, ControlInfoPropertyNames.ShowResetButton, false);
-
-      return ConfigUI;
-    }
-
-    protected override void OnCustomizeConfigUIWindowProperties(PropertyCollection Properties) {
-      Properties[ControlInfoPropertyNames.WindowHelpContentType].Value = WindowHelpContentType.PlainText;
-      Properties[ControlInfoPropertyNames.WindowHelpContent].Value = string.Format(Info.Culture, "{0}\n{1} v{2}\n{3}\nAll rights reserved.", Info.Copyright, Info.DisplayName, Info.Version, Info.Author);
-
-      base.OnCustomizeConfigUIWindowProperties(Properties);
-    }
-
-    protected override void OnSetRenderInfo(PropertyBasedEffectConfigToken NewToken, RenderArgs DstArgs, RenderArgs SrcArgs) {
-      OffsetX = NewToken.GetProperty<Int32Property>(PropertyNames.OffsetX).Value;
-      OffsetY = NewToken.GetProperty<Int32Property>(PropertyNames.OffsetY).Value;
-      Zoom = NewToken.GetProperty<Int32Property>(PropertyNames.Zoom).Value;
-      CheckMode = (byte)((int)NewToken.GetProperty<StaticListChoiceProperty>(PropertyNames.CheckMode).Value);
-      Divisor = NewToken.GetProperty<Int32Property>(PropertyNames.Divisor).Value;
-      BitMode = (byte)((int)NewToken.GetProperty<StaticListChoiceProperty>(PropertyNames.BitMode).Value);
-      UseHSV = NewToken.GetProperty<BooleanProperty>(PropertyNames.UseHSV).Value;
-      Color = ColorBgra.FromUInt32(unchecked((uint)NewToken.GetProperty<Int32Property>(PropertyNames.Color).Value));
-      base.OnSetRenderInfo(NewToken, DstArgs, SrcArgs);
-    }
-
-    protected override void OnRender(Rectangle[] Rectangles, int StartIndex, int Length) {
-      for (int i = StartIndex; i < StartIndex + Length; Render(SrcArgs.Surface, DstArgs.Surface, Rectangles[i++]));
-    }
-
-    private void Render(Surface Source, Surface Destination, Rectangle Rectangle) {
-      int Magic; 
-
-      for (int y = Rectangle.Top; y < Rectangle.Bottom && !IsCancelRequested; y++) for (int x = Rectangle.Left; x < Rectangle.Right; x++) {
-        Magic = (BitModeOptions) BitMode switch {
-          BitModeOptions.XOR => (x / Zoom + OffsetX) ^ (y / Zoom + OffsetY),
-          BitModeOptions.AND => (x / Zoom + OffsetX) & (y / Zoom + OffsetY),
-          BitModeOptions.OR  => (x / Zoom + OffsetX) | (y / Zoom + OffsetY)
-        };
-
-        if ((CheckModeOptions) CheckMode switch { CheckModeOptions.IsPrime => Utils.Math.IsPrime(Magic), CheckModeOptions.IsDivisible => Utils.Math.IsDivisible(Magic, Divisor)}) // Checked
-          if (UseHSV) 
-            Destination[x, y] = Utils.Color.FromHSV((int) (Math.Sin(Magic) * 180 + 180), 100, (int) (Math.Cos(Magic) * 50 + 50));
-          else // !HSV
-            Destination[x, y] = Color;
-        else // !Checked
-          Destination[x, y] = Source[x, y];
+    for (pos.Y = 0; pos.Y < outputRegion.Height && !IsCancelRequested; pos.Y++) {
+      for (pos.X = 0; pos.X < outputRegion.Width; pos.X++) {
+        int magic = Settings.Operation(Settings.CameraParameters.ApplyTo(pos));
+        outputRegion[pos.X, pos.Y] = Settings.Filter(magic) ? Settings.Painter(magic).GetSrgb() : sourceRegion[pos.X, pos.Y];
       }
     }
   }
+  
+  protected override PropertyCollection OnCreatePropertyCollection() => new(
+    properties: [
+      new Int32Property(PropertyNames.OffsetX, 0, 0, ushort.MaxValue),
+      new Int32Property(PropertyNames.OffsetY, 0, 0, ushort.MaxValue),
+      new Int32Property(PropertyNames.Zoom, 1, 1, 128),
+      StaticListChoiceProperty.CreateForEnum<FilterChoice>(PropertyNames.FilterMode, 0, false),
+      new Int32Property(PropertyNames.Divisor, 1, 1, 512),
+      StaticListChoiceProperty.CreateForEnum<OperationChoice>(PropertyNames.Operation, 0, false),
+      new BooleanProperty(PropertyNames.UseHSV, true),
+      new ManagedColorProperty(PropertyNames.Color, ManagedColor.Create(SrgbColors.Black))
+    ], 
+    rules: [
+      new ReadOnlyBoundToValueRule<object,StaticListChoiceProperty>(PropertyNames.Divisor, PropertyNames.FilterMode, FilterChoice.IsPrime, false),
+      new ReadOnlyBoundToBooleanRule(PropertyNames.Color, PropertyNames.UseHSV, false)
+    ]);
+
+  protected override ControlInfo OnCreateConfigUI(PropertyCollection properties) {
+    ControlInfo configUi = CreateDefaultConfigUI(properties);
+
+    configUi.SetPropertyControlValue(PropertyNames.OffsetX, ControlInfoPropertyNames.DisplayName, "X offset");
+    configUi.SetPropertyControlValue(PropertyNames.OffsetY, ControlInfoPropertyNames.DisplayName, "Y offset");
+
+    configUi.SetPropertyControlValue(PropertyNames.Zoom, ControlInfoPropertyNames.DisplayName, "Zoom");
+
+    configUi.SetPropertyControlValue(PropertyNames.FilterMode, ControlInfoPropertyNames.DisplayName, "Filter mode");
+    configUi.FindControlForPropertyName(PropertyNames.FilterMode)!.SetValueDisplayName(FilterChoice.IsPrime, "Is prime?");
+    configUi.FindControlForPropertyName(PropertyNames.FilterMode)!.SetValueDisplayName(FilterChoice.IsDivisible, "Is divisible?");
+
+    configUi.SetPropertyControlValue(PropertyNames.Divisor, ControlInfoPropertyNames.DisplayName, "Divisor");
+
+    configUi.SetPropertyControlValue(PropertyNames.Operation, ControlInfoPropertyNames.DisplayName, "Operation");
+    configUi.FindControlForPropertyName(PropertyNames.Operation)!.SetValueDisplayName(OperationChoice.XOR, "XOR");
+    configUi.FindControlForPropertyName(PropertyNames.Operation)!.SetValueDisplayName(OperationChoice.AND, "AND");
+    configUi.FindControlForPropertyName(PropertyNames.Operation)!.SetValueDisplayName(OperationChoice.OR, "OR");
+
+    configUi.SetPropertyControlValue(PropertyNames.UseHSV, ControlInfoPropertyNames.DisplayName, "");
+    configUi.SetPropertyControlValue(PropertyNames.UseHSV, ControlInfoPropertyNames.Description, "Use HSV conversion");
+
+    configUi.SetPropertyControlValue(PropertyNames.Color, ControlInfoPropertyNames.DisplayName, "Pattern color");
+    configUi.SetPropertyControlType(PropertyNames.Color, PropertyControlType.ColorWheel);
+    configUi.SetPropertyControlValue(PropertyNames.Color, ControlInfoPropertyNames.ShowResetButton, false);
+
+    return configUi;
+  }
+  
+  protected override void OnCustomizeConfigUIWindowProperties(PropertyCollection properties) {
+    properties[ControlInfoPropertyNames.WindowHelpContentType]!.Value = WindowHelpContentType.PlainText;
+    properties[ControlInfoPropertyNames.WindowHelpContent]!.Value = string.Format(PluginSupportInfo.Culture, "{0}\n{1} v{2}\n{3}\nAll rights reserved.", Info.Copyright, Info.DisplayName, Info.Version, Info.Author);
+
+    base.OnCustomizeConfigUIWindowProperties(properties);
+  }
+}
+
+internal static class MathHelpers {
+  // That's how it was originally written in Utils.Math
+  public static bool IsPrime(int num) {
+    if ((num <= 1) || (num % 2 == 0 && num > 2)) return false;
+    for (int i = 3; i < Math.Floor(Math.Sqrt(num)); i += 2)
+      if (num % i == 0)
+        return false;
+    return true;
+  }
+  public static bool IsDivisible(int num, int divisor) => num % divisor == 0;
+}
+
+internal sealed class PluginSupportInfo : IPluginSupportInfo {
+  // These should be also static, but oh well, we're implementing an interface.
+  // I wonder if there's a way to set these up without the attribute.
+  public string Author => Constants.Author;
+  public string Copyright => Constants.Copyright;
+  public string DisplayName => Constants.EffectName;
+  public Version Version { get; } = new(Constants.Version);
+  public Uri WebsiteUri { get; } = new(Constants.Website);
+  
+  public static CultureInfo Culture { get; } = new(Constants.Culture);
+  // Oops. The stream is not disposed. Who cares in this case?
+  public Image Icon { get; } = Image.FromStream(new System.IO.MemoryStream(Convert.FromBase64String(Constants.Base64Image), 0, Constants.Base64ImageStringLength), true);
+  public static string SubMenu => SubmenuNames.Render;
 }
